08e7937eb373e6f515b396d7d47fc86f467ab2da
Snailware <Adamlancaster.al@gmail.com>
2022-02-16 11:17:28 -0500

    added unit test for TimeOps method.

---
+++ DigiviceEmulator/DigiviceEmulatorLibTests/TimeOpsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DigiviceEmulatorLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DigiviceEmulatorLib.Tests
+{
+	[TestClass()]
+	public class TimeOpsTests
+	{
+		[TestMethod()]
+		public void GetElapsedSecondsTest()
+		{
+			DateTime startTime = DateTime.UtcNow;
+			Thread.Sleep(1000);
+			DateTime stopTime = DateTime.UtcNow;
+			int elapsedSeconds = TimeOps.GetElapsedSeconds(startTime, stopTime);
+			Assert.AreEqual(expected: 1,
+							actual: elapsedSeconds);
+		}
+	}
+}
