2cfd02a6ff1c42ef19f34ac23b1e4a78a2f26f05
Snailware <Adamlancaster.al@gmail.com>
2022-02-17 22:11:48 -0500

    adding SecurityOps & unit tests.

---
+++ DigiviceEmulator/DigiviceEmulatorLibTests/SecurityOpsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DigiviceEmulatorLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiviceEmulatorLib.Tests
+{
+	[TestClass()]
+	public class SecurityOpsTests
+	{
+		[TestMethod()]
+		public void HashStringTest()
+		{
+			string password = "testPassword";
+			string hash = SecurityOps.HashString(password);
+			Assert.AreNotEqual(password, hash);
+		}
+
+		[TestMethod()]
+		public void VerifyHashTest()
+		{
+			string hash1 = "testPassword";
+			string hash2 = "testPassword";
+			Assert.IsTrue(SecurityOps.VerifyHash(hash1, hash2));
+
+		}
+	}
+}
