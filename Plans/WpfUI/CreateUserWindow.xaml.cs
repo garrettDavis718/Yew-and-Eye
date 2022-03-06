bdb3db2ad6497bdf937ce104fc8cb09af5338ffa
garrettDavis718 <36609502+garrettDavis718@users.noreply.github.com>
2022-03-06 16:06:12 -0500

    Plans create user UI

---
+++ Plans/WpfUI/CreateUserWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfUI
+{
+    /// <summary>
+    /// Interaction logic for CreateUserWindow.xaml
+    /// </summary>
+    public partial class CreateUserWindow : Window
+    {
+        public CreateUserWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Check if user exists already,
+
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
