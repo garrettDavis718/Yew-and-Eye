3cecb60d7a43eb8d750240e4e5bb099e56babd0e
Snailware <Adamlancaster.al@gmail.com>
2022-03-21 19:35:23 -0400

    debugged.

--- DigiviceEmulator/DigiviceEmulatorLib/Monster.cs
+++ DigiviceEmulator/DigiviceEmulatorLib/Monster.cs
@@ -3,58 +3,273 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DigiviceEmulatorLib
 {
 	/// <summary>
-	/// monster object.
+	/// monster class.
 	/// </summary>
-	public class Monster
+	public static class Monster
 	{
 		/// <summary>
-		/// Monster object. 
+		/// monster attributes. 
 		/// </summary>
-		/// <param name="name">user-given name.</param>
-		/// <param name="animations">animations for given monster.</param>
-		/// <param name="health">current level of health of monster. (higher is better.)</param>
-		/// <param name="mood">current mood of monster. (higher is better.)</param>
-		/// <param name="dateOfBirth">date and time of monsters birth.</param>
-		public Monster(string name,
-					   AnimationSet animations,
-					   int health,
-					   int mood,
-					   DateTime dateOfBirth)
+		public enum Attributes
 		{
-			Name = name;
-			Animations = animations;
-			Health = health;
-			Mood = mood;
-			DateOfBirth = dateOfBirth;
+			name,
+			animation,
+			type,
+			state,
+			health,
+			mood,
+			hygiene,
+			date_of_birth,
+		}
+
+		/// <summary>
+		/// monster types. corresponds to bestiary entries.
+		/// </summary>
+		public enum Types
+		{
+			Faunamon,
+		}
+
+		/// <summary>
+		/// state of monster. used by animation classes. 
+		/// </summary>
+		public enum States
+		{
+			idle,
+			eating,
+			playing,
+			dancing
+		}
+
+		/// <summary>
+		/// restrict input to between 2 numbers. 
+		/// </summary>
+		/// <param name="input">input to be evaluated.</param>
+		/// <returns>returns input restricted between min and max values.</returns>
+		private static int RestrictInputRange(int input)
+		{
+			int maxValue = 100,
+				minValue = 0,
+			    restrictedInt;
+			if (input > maxValue)
+			{
+				restrictedInt = maxValue;
+			}
+			else if (input < minValue)
+			{
+				restrictedInt = minValue;
+			}
+			else
+			{
+				restrictedInt = input;
+			}
+			return restrictedInt;
 		}
 
 		/// <summary>
 		/// user-given name.
 		/// </summary>
-		public string Name { get; }
+		public static string Name
+		{
+			get 
+			{
+				string val = "";
+				if (DatabaseOps.OpenConnection())
+				{
+					val = DatabaseOps.GetMonsterAttribute<string>(Monster.Attributes.name);
+					DatabaseOps.CloseConnection();
+				}
+				return val; 
+			}
+			set 
+			{ 
+				if (DatabaseOps.OpenConnection())
+				{
+					DatabaseOps.SetMonsterAttribute(Monster.Attributes.name, value);
+					DatabaseOps.CloseConnection();
+				}
+			}
+		}
 
 		/// <summary>
 		/// animations for given monster.
 		/// </summary>
-		public AnimationSet Animations { get; set; }
+		public static AnimationSet Animations
+		{
+			get
+			{
+				AnimationSet val = null;
+				if (DatabaseOps.OpenConnection())
+				{
+					val = JsonSerializer.Deserialize<AnimationSet>(DatabaseOps.GetMonsterAttribute<string>(Monster.Attributes.animation));
+					DatabaseOps.CloseConnection();
+				}
+				return val;
+			}
+			set
+			{
+				if (DatabaseOps.OpenConnection())
+				{
+					DatabaseOps.SetMonsterAttribute(Monster.Attributes.animation, JsonSerializer.Serialize(value));
+					DatabaseOps.CloseConnection();
+				}
+			}
+		}
+
+		/// <summary>
+		/// type of monster. corresponds to bestiary entries.
+		/// </summary>
+		public static Types Type
+		{
+			get
+			{
+				Types val = 0;
+				if (DatabaseOps.OpenConnection())
+				{
+					val = (Monster.Types)DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.type);
+					DatabaseOps.CloseConnection();
+				}
+				return val;
+			}
+			set
+			{
+				if (DatabaseOps.OpenConnection())
+				{
+					DatabaseOps.SetMonsterAttribute(Monster.Attributes.type, (int)value);
+					DatabaseOps.CloseConnection();
+				}
+			}
+		}
+
+		/// <summary>
+		/// current state of monster. used by animation classes.
+		/// </summary>
+		public static States State
+		{
+			get
+			{
+				States val = 0;
+				if (DatabaseOps.OpenConnection())
+				{
+					val = (Monster.States)DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.state);
+					DatabaseOps.CloseConnection();
+				}
+				return val;
+			}
+			set
+			{
+				if (DatabaseOps.OpenConnection())
+				{
+					DatabaseOps.SetMonsterAttribute(Monster.Attributes.state, (int)value);
+					DatabaseOps.CloseConnection();
+				}
+			}
+		}
 
 		/// <summary>
 		/// current level of health of monster. (higher is better.)
 		/// </summary>
-		public int Health { get; set; }
+		public static int Health
+		{
+			get
+			{
+				int val = 0;
+				if (DatabaseOps.OpenConnection())
+				{
+					val = DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.health);
+					DatabaseOps.CloseConnection();
+				}
+				return val;
+			}
+			set
+			{
+				if (DatabaseOps.OpenConnection())
+				{
+					DatabaseOps.SetMonsterAttribute(Monster.Attributes.health, RestrictInputRange(value));
+					DatabaseOps.CloseConnection();
+				}
+			}
+		}
 
 		/// <summary>
 		/// current mood of monster. (higher is better.)
 		/// </summary>
-		public int Mood { get; set; }
+		public static int Mood
+		{
+			get
+			{
+				int val = 0;
+				if (DatabaseOps.OpenConnection())
+				{
+					val = DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.mood);
+					DatabaseOps.CloseConnection();
+				}
+				return val;
+			}
+			set
+			{
+				if (DatabaseOps.OpenConnection())
+				{
+					DatabaseOps.SetMonsterAttribute(Monster.Attributes.mood, RestrictInputRange(value));
+					DatabaseOps.CloseConnection();
+				}
+			}
+		}
+
+		/// <summary>
+		/// current level of hygiene. (higher is better.)
+		/// </summary>
+		public static int Hygiene
+		{
+			get
+			{
+				int val = 0;
+				if (DatabaseOps.OpenConnection())
+				{
+					val = DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.hygiene);
+					DatabaseOps.CloseConnection();
+				}
+				return val;
+			}
+			set
+			{
+				if (DatabaseOps.OpenConnection())
+				{
+					DatabaseOps.SetMonsterAttribute(Monster.Attributes.hygiene, RestrictInputRange(value));
+					DatabaseOps.CloseConnection();
+				}
+			}
+		}
 
 		/// <summary>
 		/// date and time of monsters birth.
 		/// </summary>
-		public DateTime DateOfBirth { get; }
+		public static DateTime DateOfBirth
+		{
+			get
+			{
+				DateTime val = DateTime.UtcNow;
+				if (DatabaseOps.OpenConnection())
+				{
+					val = DatabaseOps.GetMonsterAttribute<DateTime>(Monster.Attributes.date_of_birth);
+					DatabaseOps.CloseConnection();
+				}
+				return val;
+			}
+			set
+			{
+				if (DatabaseOps.OpenConnection())
+				{
+					DatabaseOps.SetMonsterAttribute(Monster.Attributes.date_of_birth, value);
+					DatabaseOps.CloseConnection();
+				}
+			}
+		}
 	}
 }
--- DigiviceEmulator/DigiviceEmulatorLibTests/DatabaseOpsTests.cs
+++ DigiviceEmulator/DigiviceEmulatorLibTests/DatabaseOpsTests.cs
@@ -13,6 +13,14 @@ namespace DigiviceEmulatorLib.Tests
 	public class DatabaseOpsTests
 	{
 		public static User User { get; set; } = new User("testEmail", "testHash");
+		public static string name = "testName",
+							 animations = "testAnimations";
+		public static Monster.Types type = Monster.Types.Faunamon;
+		public static Monster.States state = Monster.States.idle;
+		public static int health = 10,
+						  mood = 10,
+						  hygiene = 10;
+		public static DateTime dateOfBirth = new DateTime(1969, 4, 20);
 
 		[TestMethod()]
 		public void OpenConnectionTest()
@@ -59,6 +67,24 @@ namespace DigiviceEmulatorLib.Tests
 		}
 
 		[TestMethod()]
+		public void SetMonsterAttributeTest()
+		{
+			DatabaseOps.DatabasePath = @"../../UnitTests.db";
+			if (DatabaseOps.OpenConnection())
+			{
+				Assert.IsTrue(DatabaseOps.SetMonsterAttribute(Monster.Attributes.name, name));
+				Assert.IsTrue(DatabaseOps.SetMonsterAttribute(Monster.Attributes.animation, animations));
+				Assert.IsTrue(DatabaseOps.SetMonsterAttribute(Monster.Attributes.type, (int)type));
+				Assert.IsTrue(DatabaseOps.SetMonsterAttribute(Monster.Attributes.state, (int)state));
+				Assert.IsTrue(DatabaseOps.SetMonsterAttribute(Monster.Attributes.health, health));
+				Assert.IsTrue(DatabaseOps.SetMonsterAttribute(Monster.Attributes.mood, mood));
+				Assert.IsTrue(DatabaseOps.SetMonsterAttribute(Monster.Attributes.hygiene, hygiene));
+				Assert.IsTrue(DatabaseOps.SetMonsterAttribute(Monster.Attributes.date_of_birth, dateOfBirth));
+				DatabaseOps.CloseConnection();
+			}
+		}
+
+		[TestMethod()]
 		public void DeleteUserTest()
 		{
 			DatabaseOps.DatabasePath = @"../../UnitTests.db";
@@ -80,6 +106,24 @@ namespace DigiviceEmulatorLib.Tests
 				string passwordHash = DatabaseOps.GetPasswordHash(User.Email);
 				Assert.AreEqual(passwordHash, "testHash");
 				DatabaseOps.DeleteUser(User);
+				DatabaseOps.CloseConnection();
+			}
+		}
+
+		[TestMethod()]
+		public void GetMonsterAttributeTest()
+		{
+			DatabaseOps.DatabasePath = @"../../UnitTests.db";
+			if (DatabaseOps.OpenConnection())
+			{
+				Assert.AreEqual(name, DatabaseOps.GetMonsterAttribute<string>(Monster.Attributes.name));
+				Assert.AreEqual(animations, DatabaseOps.GetMonsterAttribute<string>(Monster.Attributes.animation));
+				Assert.AreEqual((int)type, DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.type));
+				Assert.AreEqual((int)state, DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.state));
+				Assert.AreEqual(health, DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.health));
+				Assert.AreEqual(mood, DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.mood));
+				Assert.AreEqual(hygiene, DatabaseOps.GetMonsterAttribute<int>(Monster.Attributes.hygiene));
+				Assert.AreEqual(dateOfBirth, DatabaseOps.GetMonsterAttribute<DateTime>(Monster.Attributes.date_of_birth));
 				DatabaseOps.CloseConnection();
 			}
 		}
