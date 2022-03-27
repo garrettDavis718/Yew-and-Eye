2c49e80b947c50f43eca5d041c51eb0795d8477b
Trace_Code89 <71043578+TraceyP89@users.noreply.github.com>
2022-03-27 16:57:27 -0400

    maps

---
+++ Plans/WpfUI/MapsWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
+namespace WpfUI
+{
+    /// <summary>
+    /// Interaction logic for MapsWindow.xaml
+    /// </summary>
+    public partial class MapsWindow : Window
+    {
+        public MapsWindow()
+        {
+            InitializeComponent();
+
+            PlansMap.Mode = new AerialMode(true);
+        }
+
+
+    }
+}
